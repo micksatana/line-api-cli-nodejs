11a9170ea56f0c175bf0baf60046ee69b10ad0c4
somsakra <somsakra@live.com>
2020-01-13 10:25:33 +0700

    Add missing bin build files

---
+++ bin/apis/linetv-list-modules-request.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _linetvRequest = _interopRequireDefault(require("./linetv-request"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class LINETvListModulesRequest extends _linetvRequest.default {
+  constructor(options) {
+    super(options);
+    this.endpoint = `${this.endpoint}/curation`;
+  }
+  /**
+    * @param {number} channelId
+    * @param {string} channelCode
+    * @return {AxiosResponse<LINETvListModulesResponseData>}
+    */
+
+
+  send(channelId, countryCode) {
+    return this.axios.get(`${this.endpoint}/list?lineChannelId=${channelId}&country=${countryCode}`);
+  }
+
+}
+
+exports.default = LINETvListModulesRequest;
+//# sourceMappingURL=linetv-list-modules-request.js.map
---
+++ bin/clis/linetv-cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+"use strict";
+
+var _colors = _interopRequireDefault(require("colors"));
+
+var _operation = _interopRequireDefault(require("../operations/operation"));
+
+var _theme = _interopRequireDefault(require("../theme"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_colors.default.setTheme(_theme.default);
+
+if (_operation.default.config) {
+  const LINETvCommand = require('../commands/linetv-command').default;
+
+  LINETvCommand.cli();
+}
+//# sourceMappingURL=linetv-cli.js.map
